917ecdccc60a394b0af0507f2baddc24d6e8963e
Shivani Garg <shivani1999garg@gmail.com>
2025-03-19 11:25:12 +0000

    Adding linux_assignment

---
+++ linux_assignments/1_cpy_assignment/main.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#define SUCCESS         0
+#define FAILURE         1
+#define BUFF_SIZE 255  // Define buffer size
+
+
+
+
+
+
+
+
